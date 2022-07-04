2a44e13e3d6d385197e5996c3c4af8be0bf6dc0c
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-07-04 23:09:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const num = prompt('Son kiriting');
+
+
+// 3 va 5 ga bo'linishi
+// if(Number(num) % 3 == 0 && Number(num) % 5 == 0) {
+//     alert('FizzBuzz')
+// } else if(Number(num) % 3 == 0) {
+//     alert('Fizz')
+// } else if(Number(num) % 5 == 0) {
+//     alert('Buzz')
+// } else {
+//     alert('ok')
+// }
+
+
+// Juft son toq son
+if(Number(num) % 1 == 0 && Number(num) % 2 == 0) {
+    alert('FizzBuzz')
+} else if(Number(num) % 1 == 0) {
+    alert('Fizz')
+} else if(Number(num) % 2 == 0) {
+    alert('Buzz')
+} else {
+    alert('ok')
+}
+
+// Islombek
